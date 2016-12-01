35dfb5ca2fad3a09e02834e82d5eb2eb6f5da335
Tara Vancil <tbvanc@gmail.com>
2016-12-01 14:03:51 -0500

    set up gulp and babel

---
+++ gulpfile.babel.js
@@ -0,0 +1,23 @@
+import gulp from 'gulp'
+import babel from 'gulp-babel'
+
+gulp.task('compile', ['compile-lib', 'compile-bin'])
+
+gulp.task('compile-lib', () => {
+  gulp.src('lib/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('build/lib'))
+})
+
+gulp.task('compile-bin', () => {
+  gulp.src('bin/*')
+    .pipe(babel())
+    .pipe(gulp.dest('build/bin'))
+})
+
+gulp.task('watch', () => {
+  gulp.watch('bin/*', ['compile-bin'])
+  gulp.watch('lib/*.js', ['compile-lib'])
+})
+
+gulp.task('default', ['compile', 'watch'])
