8ee1f1c8605215518fe678ca339698157e074974
Tara Vancil <contact@taravancil.com>
2019-12-16 15:18:51 -0600

    add prettier and set up pre-commit linting hook

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,26 +1,28 @@
-import gulp from 'gulp'
-import babel from 'gulp-babel'
+import gulp from "gulp";
+import babel from "gulp-babel";
 
-gulp.task('compile', ['compile-lib', 'compile-bin'])
+gulp.task("compile", ["compile-lib", "compile-bin"]);
 
-gulp.task('compile-lib', () => {
-  gulp.src('lib/**/*.js')
+gulp.task("compile-lib", () => {
+  gulp
+    .src("lib/**/*.js")
     .pipe(babel())
-    .pipe(gulp.dest('build/lib'))
-})
+    .pipe(gulp.dest("build/lib"));
+});
 
-gulp.task('compile-bin', () => {
-  gulp.src('bin/*')
+gulp.task("compile-bin", () => {
+  gulp
+    .src("bin/*")
     .pipe(babel())
-    .pipe(gulp.dest('build/bin'))
-})
+    .pipe(gulp.dest("build/bin"));
+});
 
-gulp.task('watch-bin', () => {
-  gulp.watch('bin/*', ['compile-bin'])
-})
+gulp.task("watch-bin", () => {
+  gulp.watch("bin/*", ["compile-bin"]);
+});
 
-gulp.task('watch-lib', () => {
-  gulp.watch('lib/**/*.js', ['compile-lib'])
-})
+gulp.task("watch-lib", () => {
+  gulp.watch("lib/**/*.js", ["compile-lib"]);
+});
 
-gulp.task('default', ['compile', 'watch-lib', 'watch-bin'])
+gulp.task("default", ["compile", "watch-lib", "watch-bin"]);
